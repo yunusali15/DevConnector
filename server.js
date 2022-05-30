3bfa2a7d9953be2f66268cf1ab977308438f86e6
Yunus <yunusali150201@gmail.com>
2022-05-30 14:41:53 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send("App Running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
