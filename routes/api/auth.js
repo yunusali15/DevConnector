1251308362b439f06739ef78900c8b26aa917f8e
Yunus <yunusali150201@gmail.com>
2022-05-30 16:03:56 +0800

    Added Routing

---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+//@route GET api/auth
+//@access Public
+//@desc Test Route
+router.get('/', (req, res)=> {res.send("Auth Route...")}); //the slash is the default route when the api endpoint hits the resource
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const  connectDb = require('./config/db');
 
 const app = express();
+
+connectDb();
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.get('/', (req, res) => res.send("App Running"));
 
