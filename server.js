c5dccf396e892e2fa66764b794d7653ea7998647
Yunus <yunusali150201@gmail.com>
2022-07-13 18:24:38 +0800

    pre-deploy edits

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const  connectDb = require('./config/db');
+const express = require("express");
+const connectDb = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 connectDb();
 
-app.get('/', (req, res) => res.send("App Running"));
-
 //Init Middleware
-app.use(express.json({extended: false})); //Code needed to get the request body when sending POST req
+app.use(express.json({ extended: false })); //Code needed to get the request body when sending POST req
 
-app.use('/api/users', require('./routes/api/users')); //matches the url to the resources path that is matched to the '/' request of the folder
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profiles', require('./routes/api/profile'));
+app.use("/api/users", require("./routes/api/users")); //matches the url to the resources path that is matched to the '/' request of the folder
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profiles", require("./routes/api/profile"));
 
+//Serve Static in production
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 const PORT = process.env.PORT || 5000;
 
