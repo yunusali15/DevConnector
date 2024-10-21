87ffe9003ba3acbe3e6b4a40730e9c71e2eb0251
Yunus <yunusali150201@gmail.com>
2024-10-22 06:20:15 +0800

    Authentication middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 
-//@route GET api/auth
+//@route GET api/posts
 //@access Public
-//@desc Test Route
-router.get('/', (req, res)=> {res.send("Auth Route...")}); //the slash is the default route when the api endpoint hits the resource
+//@desc Test Route 
+router.get('/', auth, //the slash is the default route when the api endpoint hits the resource 
+async (req, res)=> {
+    try {
+        const user = await User.findById(req.user.id).select('-password'); //searches the user in the database and returns the record without the password
+        
+        res.json(user); //returns the user in the json format
+    } catch (err) {
+        return res.status(401).json(); //401 is code for no authorisation for protected resources
+    }
+}); 
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -60,7 +60,7 @@ router.post('/', [
 
             jwt.sign(payload, 
                 config.get('jwtSecret'),
-                { expiresIn: 36000 },
+                { expiresIn: 360000 },
                 (err, token) => {
                     if(err) throw err
                     res.json({token});
